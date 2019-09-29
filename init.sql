35d70d7209f4e6d2571b02d40c090f37c22dbb9c
annickboel <annick.boel@gmail.com>
2019-09-29 18:07:25 +0200

    Add files via upload

--- init.sql
+++ init.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `username` varchar(8) UNIQUE NOT NULL,
   `password` varchar(8) NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `token` varchar(100),
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -11,16 +12,27 @@ CREATE TABLE IF NOT EXISTS `users` (
 ALTER TABLE `users` ADD PRIMARY KEY (`id`);
 ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-
-CREATE TABLE IF NOT EXISTS `ads` (
+CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `content` varchar(200) NOT NULL,
+  `name` varchar(50) UNIQUE NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-ALTER TABLE `ads` ADD PRIMARY KEY (`id`);
-ALTER TABLE `ads` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ 
+ALTER TABLE `categories` ADD PRIMARY KEY (`id`);
+ALTER TABLE `categories` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `ads` ADD FOREIGN KEY (`userId` ) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+CREATE TABLE IF NOT EXISTS `adz` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `price` decimal(6,2) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+ALTER TABLE `adz` ADD PRIMARY KEY (`id`);
+ALTER TABLE `adz` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `adz` ADD FOREIGN KEY (`userId` ) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `adz` ADD FOREIGN KEY (`categoryId` ) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
